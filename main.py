7b8edc8458b36c56958a29abe70ba84467681624
Marco <marco.tollini1@swisscom.com>
2022-04-14 10:15:52 +0200

    better IP creaton

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ command = "ip addr {operation} {ip}/{prefix} dev {interface}"
 
 def mount_ip(interface, ip, prefix, number_ips, operation = "add"):
     if '.' in ip:
-        start = ipaddress.IPv4Address(ip)
+        ip = ipaddress.IPv4Address(ip)
         network = ipaddress.IPv4Network(f'{ip}/{prefix}', strict=False)
+        max_ip = max(network)
     else:
-        start = ipaddress.IPv6Address(ip)
+        ip = ipaddress.IPv6Address(ip)
         network = ipaddress.IPv6Network(f'{ip}/{prefix}', strict=False)
-    it = network.hosts()
-    while next(it) < start:
-        continue
+        max_ip = max(network)
 
     for i in range(1, number_ips+1):
-        ip = next(it)
+        if ip > max_ip:
+            raise Exception(f'Out of mask. {i-1} ip added')
+
         os.system(command.format(ip=str(ip), prefix=prefix, interface=interface, operation=operation))
         if i % 100 == 0:
             print(f"Done {i}/{number_ips} interfaces: {operation}")
+        ip = next(ip)
+
 
     print("All done :)")
 
